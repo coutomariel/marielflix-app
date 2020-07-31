ee9a80f07b07d0620bcd48dc84c939b4acb713a3
Mariel <coutomariel@gmail.com>
2020-07-30 22:28:27 -0300

    Ajustada estrutura de pastas

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -5,7 +5,14 @@ import { Link } from 'react-router-dom'
 export default function CadastroCategoria() {
     return (
         <PageDefault>
-            <h1>Cadastro de categoria</h1>
+            <h1>Cadastro de Categoria</h1>
+            <form>
+                <label>
+                    Nome da Categoria:<input type="text"/>
+                </label>
+                <button>Cadastrar</button>
+            </form>
+
             <Link to="/">
                 Ir para home
             </Link>
